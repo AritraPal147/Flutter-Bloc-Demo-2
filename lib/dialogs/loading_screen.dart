66499303e94ed4b3ce995edd02d0f1365c89b941
Aritra Pal <94114400+AritraPal147@users.noreply.github.com>
2023-03-29 14:29:10 +0530

    completed loading screen

--- lib/dialogs/loading_screen.dart
+++ lib/dialogs/loading_screen.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc_demo2/dialogs/loading_screen_controller.dart';
 
@@ -13,13 +12,48 @@ class LoadingScreen {
   // It is used here because only one instance of this class has to be created (singleton)
   factory LoadingScreen.instance() => _shared;
 
+  // Controller variable for the loading screen
   LoadingScreenController? _controller;
-  LoadingScreenController showOverlay({
+
+  // This function will be called when we need to show overlay
+  // This is public function, it will internally call _showOverlay()
+  void show({
     required BuildContext context,
     required String text,
   }) {
-    final _text = StreamController<String>();
-    _text.add(text);
+    // ?? -> if null operator -> checks if given parameter is null or not and executes if null
+    // Here it checks if _controller?.update(text) is null or not.
+    // If null, takes false (overlay does not need to be updated / is not present on screen)
+    // and if not null takes _controller?.update(text) (overlay is already present on screen, so needs to be updated)
+    if (_controller?.update(text) ?? false) {
+      return;
+    } else {
+      _controller = _showOverlay(
+          context: context,
+          text: text,
+      );
+    }
+  }
+
+  // Hides the overlay
+  void hide() {
+    // Closes the controller -> closes the textStream and removes the overlay
+    // (look for close() function below in the return statement)
+    _controller?.close();
+    _controller = null;
+  }
+
+  // Function to show the overlay on the screen -> to be called internally
+  LoadingScreenController _showOverlay({
+    required BuildContext context,
+    required String text,
+  }) {
+    // Creates a stream controller of type String
+    // To get a stream of this controller, we use textStream.stream
+    final textStream = StreamController<String>();
+    // Asks stream to send text values as event -> basically adds the text
+    // provided to the loading screen to the stream
+    textStream.add(text);
 
     // An overlay state variable
     final state = Overlay.of(context);
@@ -50,11 +84,66 @@ class LoadingScreen {
                 ),
                 child: Padding(
                   padding: const EdgeInsets.all(16.0),
-                  child: null,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        const CircularProgressIndicator(),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        // Builds itself based on the latest snapshot of interaction with
+                        // a stream
+                        StreamBuilder<String>(
+                          // The stream used by this StreamBuilder is the stream
+                          // controlled by textStream
+                            stream: textStream.stream,
+                            // Builds on basis of a context and a
+                            // snapshot (result of the stream textStream.stream)
+                            builder: (context, snapshot) {
+                              // Checks if the stream currently contains data or not
+                              if (snapshot.hasData) {
+                                // If stream contains data, then show the data as a Text widget
+                                return Text(
+                                  snapshot.data!,
+                                  textAlign: TextAlign.center,
+                                );
+                              } else {
+                                return Container();
+                              }
+                            },
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ),
             ),
           );
+        },
+    );
+
+    // Insert the overlay into the current state
+    state.insert(overlay);
+
+    // Returns a loading screen controller with close() and update() functions
+    return LoadingScreenController(
+        close: () {
+          // closes the textStream
+          textStream.close();
+          // Removes the overlay since it is no longer required
+          overlay.remove();
+          return true;
+        },
+        update: (text) {
+          // Adds the text to be updated into the textStream
+          // so that it can be changed when required
+          textStream.add(text);
+          return true;
         },
     );
   }
